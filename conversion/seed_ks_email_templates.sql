8954995526c718ad8cc8647479bbff830eed95ff
Jorge Rimblas <jorge@rimblas.com>
2018-11-16 10:31:53 -0600

    Updated email templates

--- conversion/seed_ks_email_templates.sql
+++ conversion/seed_ks_email_templates.sql
@@ -1,8 +1,29 @@
 REM INSERTING into KS_EMAIL_TEMPLATES
 SET DEFINE OFF;
+Insert into KS_EMAIL_TEMPLATES (NAME,TEMPLATE_TEXT) values ('RESET_PASSWORD_REQUEST_NOTIFICATION','Hi #USER_FIRST_NAME#,
 
-insert into KS_EMAIL_TEMPLATES (NAME,TEMPLATE_TEXT) values ('SESSION_LOAD','The #TRACK_NAME# has received #SESSION_COUNT# sessions.
+You either have a brand new account with username #USERNAME# or a reset password has been requested for your account on the ODTUG Kscope Voting Apps.
 
-Please review or vote as needed.
-#VOTING_APP_LINK#');
+Your temporary password is: #TEMP_PASSWORD#
+You will need to change it when you log in for the first time.
+
+Click on the following link to login to the Voting App:
+#VOTING_APP_LINK#
+
+If you''re a Track Lead you can use the Abstract Review App:
+#ADMIN_APP_LINK#
+');
+Insert into KS_EMAIL_TEMPLATES (NAME,TEMPLATE_TEXT) values ('RESET_PASSWORD_DONE_NOTIFICATION','Hi #USER_FIRST_NAME#,
+Your password has been reset.
+
+If you did not reset it, please contact info@odtug.com
+');
+Insert into KS_EMAIL_TEMPLATES (NAME,TEMPLATE_TEXT) values ('SESSION_LOAD','The "#TRACK_NAME#" has received #SESSION_COUNT# sessions.
+
+If the voting period is open go vote at:
+#VOTING_APP_LINK#
+
+If you''re a Track Lead you can use the Abstract Review App:
+#ADMIN_APP_LINK#
+');
 
