ee934a6cae0d0c937576d0c4b2e3bc296639a6f8
Jorge Rimblas <jorge@rimblas.com>
2019-10-25 18:36:51 -0500

    Implemented side bad region toggle

---
+++ www/src/js/p2.js
@@ -0,0 +1,43 @@
+var scrollsave = 0,
+    sideBodyState = "open";
+
+// Derived from @J_Snyders code
+// Set title of the dialog Session Details
+$("body").on("dialogcreate", ".ui-dialog--apex", function(e) {
+    $(this).closest(".session-details-modal").children(".ui-dialog-content").dialog("option", "title", $v("P2_DIALOG_TITLE"));
+});
+
+// Trigger the toggle when the NavBar is changed
+apex.jQuery("#t_TreeNav").on('theme42layoutchanged', function(event, obj) {
+    toggleBodySide(sideBodyState);
+});
+
+function toggleBodySide(state) {
+  var $side=$(".t-Body-side"),
+      $body=$(".t-Body-content"),
+      $expandBtn=$("#expandTags"),
+
+      closedMargin=245,
+      closeSide=0,
+      openMargin=480,
+      openSide=240;
+
+  sideBodyState = state;
+h
+  if ($("body").hasClass("js-navCollapsed")) {
+    openMargin=280;
+    closedMargin=40;
+  }
+
+  if (state === "close"){
+    $side.animate({width: closeSide}, 'fast', function(){$expandBtn.show(); });
+    $body.animate({'margin-left': closedMargin}, 'fast', function(){$expandBtn.show(); });
+  }
+  else {
+    $side.animate({width: openSide}, 'fast');
+    $body.animate({'margin-left': openMargin}, 'fast');
+    $expandBtn.hide();   
+  }
+  apex.event.trigger(window, 'resize');
+
+}
